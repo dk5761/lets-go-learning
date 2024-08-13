a7d87d9b1fb83127e203f4e30ef177763f34ef62
Darshan Kadam <darshan.kadam@ramp-ai.com>
2024-08-13 17:29:02 +0530

    4

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"lets.go/internals/models"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network Address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
 	flag.Parse()
 
@@ -22,9 +27,20 @@ func main() {
 	errorLog := log.New(os.Stderr, "ERROR\t",
 		log.Ldate|log.Ltime|log.Lshortfile)
 
+
+	db, err := openDB(*dsn)
+	if err!=nil{
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -34,7 +50,20 @@ func main() {
 	}
 	infoLog.Printf("Starting server on %s", *addr)
 	// Call the ListenAndServe() method on our new http.Server struct.
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 
+}
+
+func openDB(dsn string) (*sql.DB, error){
+	db, err :=sql.Open("mysql", dsn)
+	if err != nil{
+		return nil, err
+	}
+
+	if err = db.Ping(); err!=nil{
+		return nil, err
+	}
+
+	return db, nil
 }
